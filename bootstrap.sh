1ef0a71279f0413d3695728237c4c4e66cb9cd88
Sebastian Humberg <36735186+shmbrg@users.noreply.github.com>
2024-04-15 14:54:22 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,56 +1,35 @@
 #!/bin/bash
 
-# install Homebrew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# general
+# General
 brew install zsh;
 brew install iterm2;
-#brew install httpie;
 brew install tree;
 brew install gh;
-#brew install speedtest --force;
-#brew install openssl;
 
-# java
-#brew install java11;
-#sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk;
+# Java
+brew install openjdk@11;
 
-# dotfile manager
+# Dotfile manager
 brew install chezmoi;
 
-# env management
+# Env management
 brew install pyenv;
 brew install tfenv;
-# install poetry from scratch, brew will install python3.10 as dependency, which will be used in every venv
-#curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 
 # Code
 brew install --cask pycharm-ce;
 brew install --cask visual-studio-code;
-#brew install jupyter;
-#brew tap dbt-labs/dbt,
-#brew install dbt;
 
 # Container
 brew install --cask docker;
-#brew install minikube;
-#brew install hyperkit; # VM driver
-#brew install helm;
-#brew install --cask rancher;
-
-# Cloud
-#brew install --cask google-cloud-sdk;
-#brew install vault;
-#brew install --no-dependencies azure-cli;
-
-# install aws cli
-#curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-#sudo installer -pkg AWSCLIV2.pkg -target /
-#rm AWSCLIV2.pkg
+# brew install helm;
 
 # Browsers
-#brew cask install google-chrome;
+brew cask install google-chrome;
 
 # Convenience
 brew install --cask bettertouchtool;
